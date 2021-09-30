6248a6a7f2c4be3fe84f95647e76fef66ef3dfeb
testaccount <test@gmail.com>
2021-10-01 01:19:14 +0900

    until event.preventDefault

--- momentum/app.js
+++ momentum/app.js
@@ -1,33 +1,65 @@
+// const loginForm = document.getElementById("login-form"); //querySelector("#login-form")
+// const loginInput = loginForm.querySelector("input"); 
+// const loginButton = loginForm.querySelector("button"); 아래 두줄로 만들수도 있음
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+//const loginButton = document.querySelector("#login-form button"); -->form태그 덕분에 버튼신경안써도 됨
+
+//function onLoginBtnClick(){
+    //console.log("click");
+    //console.dir(loginInput);
+    //console.log("hello", loginInput.value);
+    //const username = loginInput.value;
+    // if(username ===""){
+    //     alert("Please write your name");
+    // }else if(username.length > 15){
+    //     alert("Your name is too long.");
+    // }  --> html input태그에서 다 제한 했음
+    //console.log(username);
+
+//}
+
+//loginButton.addEventListener("click", onLoginBtnClick); -->form태그 덕분에 버튼신경안써도 됨
+
+// function onLoginSubmit(){
+//     const username = loginInput.value;
+//     console.log(username);
+// }
+
+// funciton onLoginSubmit(tomato){
+   //tomato.preventDefault();
+   //console.log(tomato);}
+   //JS는 함수를 실행시키는 동시에 그 함수에 첫번째 인자로 object를 넣어줌
+   //우리는 받기만 하면 됨
+
+   //브라우저가 기본 동작을 실행하지 못하게 막아주는 것
+   //(ex.form태그 안에서 input enter or button click -> 자동 새로고침)
+function onLoginSubmit(event){
+   evnet.preventDefault();
+   console.log(event);
+   console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+//function에서 onLoginSubmit()는 브라우저가 자동으로 즉시 function을 실행시켜버린다는 뜻
+// **************eventListner에서의 함수는 내가 직접 실행하는것이 아닌 
+//                  JS가 실행시켜주고 event에 대한 정보도 담아준다 **************
 
 
-const h1 = document.querySelector("div.hello:first-child h1");
-//h1.style.color = "blue";
+// link click의 기본 동작은 클릭하면 사이트가 넘어감 그 동작을 막아보자
+const link = document.querySelector("a");
 
-function handleh1Click(){
-    // console.log(h1.style.color);
-    // h1.style.color = "purple";
-    // console.log(h1.style.color); --> css
-
-    // const clickedClass = "clicked";
-    // if(h1.className === clickedClass){
-    //     h1.className = "";
-    // }else {
-    //     h1.className = clickedClass;
-    // }   --> css에 클래스 네임 명시해주면 그건 무시당하는 버그가 생길수 있음. sexy-font가 replace됨
-
-    // const clickedClass = "clicked";
-    // if(h1.classList.contains(clickedClass)){
-    //     h1.classList.remove(clickedClass);
-    // }else {
-    //     h1.classList.add(clickedClass);
-    // }
-
-    
-    h1.classList.toggle("clicked");
+function handleLinkClick(event){
+    event.preventDefault();
+    console.log(event); // event에 어떤 정보가 담겨있는지 알수있어 mouse event
+    console.dir(event);
+    alert("clicked!");
+    //alert 후에 ok 누르면 기본동작 실행됨
 }
 
 
 
+link.addEventListener("click", handleLinkClick);
 
-
-h1.addEventListener("click", handleh1Click);
+//JS는 함수를 실행시키는 동시에 그 함수에 첫번째 인자로 object를 넣어줌
