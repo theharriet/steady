9ffe0d7484c2d6de439f67a9db2a394ad59ec388
theharriet <goand123@gmail.com>
2021-10-02 01:02:15 +0900

    paint todolist from the local storage

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -1,34 +1,29 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("input");
-// const toDoInput = document.querySelector("#todo-form input"); same~~~~
+const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.getElementById("todo-list");
 
-const toDos = []; //newTodo가 그려질때마다(paintTodo()) 그 텍스트를 배열에 push
+const TODOS_KEY = "todos";
+
+let toDos = [];  //const toDos = [];
+//newTodo가 그려질때마다(paintTodo()) 그 텍스트를 배열에 push
 //handleToDoSubmit에서 paintTodo하기 전에 toDos array를 가져와서 newTodo push
 
 //local Storage에는 배열 저장불가. only String can be saved.
 function saveToDos(){
     //localStorage.setItem("todos", toDos);
-    localStorage.setItem("todos", JSON.stringify(toDos));
-    //****JSON.stringify() = 배열이든 object든 다 string으로 만들어줌
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+    // ****JSON.stringify() = 배열이든 object든 다 string으로 만들어줌
     //이제 localstorage안에 string으로 된 배열이 들어가있음 ex) ["a", "b", "c"]
     // 그 전에는 a,b,c,.. 단순 텍스트로 들어가있었음
-
 
 }
 
 function deleteTodo(event){
-    // console.log(event); //어떤버튼이 눌렸는지 볼수있음
-    // console.dir(event.target); //parentNode, parentNode 확인가능
-    // console.dir(event.target.parentElement.innerText); //뭐라고 써져있는지도 나옴
-    // console.log(event.target.parentElement); //li들 확인가능
     const li = event.target.parentElement;
     li.remove();
 }
 
 function paintToDo(newTodo){
-    //console.log("I will paint", newTodo);
-    //li만들어서 ul 아래에 넣기
     const li = document.createElement("li");
     const span = document.createElement("span"); //나중에 button만들어서 삭제하려고 span태그까지 넣어줌
     span.innerText = newTodo;
@@ -37,23 +32,40 @@ function paintToDo(newTodo){
     button.addEventListener("click", deleteTodo);
     li.appendChild(span);
     li.appendChild(button);
-    
-    //console.log(li);
     toDoList.appendChild(li);
 }
-// 새로고침하면 사라짐 -> local storage에 저장, 삭제 버튼없음 -> deleteTodo
 
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
-    //console.log(toDoInput.value);
     toDoInput.value = "";
-    //console.log(newTodo, toDoInput.value);
 
     toDos.push(newTodo);
     paintToDo(newTodo);
     saveToDos();
-
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+//로컬스토리지에서 가져와서 화면에 아직 못 뿌려줌
+//from the local storage에서 JSON.pars 해서 가져올거야
+const savedToDos = localStorage.getItem(TODOS_KEY);
+//console.log(savedToDos);
+if(savedToDos !== null){ //if(savedToDos) => local storage에 todos가 있다면.
+    const parseToDos = JSON.parse(savedToDos); //단순 텍스트를 배열형태로 변환 (object형태?)
+    //console.log(savedToDos);
+    // parseToDos.forEach(sayHello); //array에 있는 각각의 item에 대해 function(sayHello)을 실행
+    //sayHello function 만드는 대신에
+    //parseToDos.forEach((item) => console.log("this is the turn of ", item));
+    // function을 더 짧게 작성하는 방법. (화살표함수)
+    // function sayHello(item){ //JS가 event처럼 제공해주는 argument 정보
+    //     console.log("this is the turn of ", item);
+    // } 똑같
+    
+    //지금 문제는 local storage에 새로고침하고 다시 todos 쓰면 replacing 돼
+    // 이유 -> todos배열 선언할때 빈배열이였고 handletodosubmit에서 빈배열에 push하고있어서
+    // 해결-> application이 시작될 때 todos array를 빈 array가 아닌
+    // const를 let으로 바꿔서 업뎃이 가능하도록 만들어 
+    toDos = parseToDos; // 이전 배열을 저장하고 있는 array로 변경하고
+    parseToDos.forEach(paintToDo); //paintToDo 실행
+}
