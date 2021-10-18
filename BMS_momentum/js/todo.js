4aac1c7546c10abbcf1596ed8a599c13d8742c0f
theharriet <goand123@gmail.com>
2021-10-18 22:51:00 +0900

    todos

--- BMS_momentum/js/todo.js
+++ BMS_momentum/js/todo.js
@@ -1,3 +1,5 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
