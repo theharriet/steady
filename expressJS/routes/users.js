8d5c3cb87ff04921493ee9896969b2f343b896bd
theharriet <goand123@gmail.com>
2021-10-27 11:27:28 +0900

    param and logger

--- expressJS/routes/users.js
+++ expressJS/routes/users.js
@@ -2,24 +2,68 @@ const express = require("express")
 const router = express.Router()
 // router is mini app, independence. app처럼 get과 post를 갖고있음
 
+//that means every singl route 쓰면 logger 작동 -> main 페이지에서는 안됨
+router.use(logger)
+
+// http://localhost:3000/users
 router.get("/", (req, res) => {
     res.send("User List")
  })
 
- router.get("/new", (req, res)=> {
+ // http://localhost:3000/users/new
+ // 얘가 /:id보다 아래에 있으면 /new도 /:id로 뜸. 코드를 위에서부터 읽기때문에.
+router.get("/new", (req, res) => {
     res.send("User New Form")
  })
 
+router.post("/", (req, res) =>{
+    res.send("Create User")
+})
+
+//아래 세개의 router랑 exactly same
+router.route("/:id")
+.get((req, res) => {
+    console.log(req.user)
+    res.send(`Get User With ID ${req.params.id}`)
+})
+.put((req, res) => {
+    res.send(`Put User With ID ${req.params.id}`)
+})
+.delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`)
+})
+
+const users = [{ name: "Harriet"}, { name: "Sally"}]
+
+// whenever you find parameter with the name of 'id', run this function
+router.param("id", (req, res, next, id) => {
+    //console.log(id)
+    req.user = users[id]
+    next()
+})
+//what this param has allowed router to do -> anytime we have an 'id' get it from our user(we have 
+//'users' variable), get the particular user for that 'id', then continue on with the rest of our code(next()) 
+
+function logger(req, res, next){
+    console.log(req.originalUrl)
+    next()
+}
+
+// // dynamic parameter. /를 포함한 모든 url ex) /1, /2, /1424
+// router.get("/:id", (req, res) => {
+//     //req.params.id
+//     res.send(`Get User With ID ${req.params.id}`)
+// })
+
+// router.put("/:id", (req, res) => {
+//     res.send(`Put User With ID ${req.params.id}`)
+// })
+
+// router.delete("/:id", (req, res) => {
+//     res.send(`Delete User With ID ${req.params.id}`)
+// })
+// //get, put, delete 이 세개의 router는 almost same
+
+
  module.exports = router
 
-// 이 라우터 두개는 "/users"를 다 갖고 있으므로 생략해줌
-// app.get() 대신에 router.get() or router.post() 써줌
-// // http://localhost:3000/users
-// router.get("/users", (req, res) => {
-//     res.send("User List")
-// })
-
-// // http://localhost:3000/users/new
-// router.get("/users/new", (req, res)=> {
-//     res.send("User New Form")
-// })
--- expressJS/server.js
+++ expressJS/server.js
@@ -1,46 +1,27 @@
-//console.log("hello");
 const express = require("express")
 const app = express()
-// it means it is main application 
 
-//npm i ejs 후에
+//app.use(logger) //all of your routes에 쓰고 싶으면 페이지 젤 위에 쓰래(everything is top to bottom), 
+//개별적으로 쓸수도있음 - app.get("/", logger, (req, res))
+//users.js에다가 쓸수도 있음
+
 app.set("view engine", "ejs")
 
 
-// http://localhost:3000
+//get or post never use 'next'
 app.get("/", (req, res) => {
     console.log("Here")
-    //res.send("Hi")
-    //res.sendStatus(500) - 500 internal server error
-    //res.status(500).send("Hi") - 500 에러 나면서 화면엔 hi
-    //res.status(500).json({ message : "Error"})
-    //res.json({ message : "Error"})
-    //res.download("server.js") // user가 파일을 다운로드할수 있게끔. 파일 path써주면됨
     res.render("index", { text: "World"}) // html or ejs 파일 랜더링
 })
 
-//user.js로 옮김
-// //페이지가 하나씩 만들어짐 http://localhost:3000/users
-// app.get("/users", (req, res) => {
-//     res.send("User List")
-// })
-
-// // http://localhost:3000/users/new
-// app.get("/users/new", (req, res)=> {
-//     res.send("User New Form")
-// })
-
 const userRouter = require("./routes/users")
-//const postRouter = require("./routes/users")
 
 app.use("/users", userRouter) //users.js의 두 라우터가 /users로 시작하므로 생략한 부분 설정
-//app.use("/users", postRouter)
+
+//users.js로 옮김
+// function logger(req, res, next){
+//     console.log(req.originalUrl)
+//     next()
+// }
 
 app.listen(3000)
-
-
-// npm init -y
-// npm i express
-// npm i --save-dev nodemon 서버를 언제든 리스타트하기 쉽게 만들어줌
-// npm run devStart
-// npm i ejs
